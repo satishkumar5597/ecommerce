28b9cd470c17aa8c3f46dc7365871b08c1cb6ca2
satishkumar5597 <satishkumar.s@credopay.com>
2023-12-12 10:36:27 +0530

    header-section-footer-created

--- ecommerce/src/app/app.component.ts
+++ ecommerce/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SectionComponent } from './layout/section/section.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FooterComponent,HeaderComponent,SectionComponent,FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
